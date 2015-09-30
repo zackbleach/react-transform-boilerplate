b08c5f662bd406c21b2441b031e55537496a9cb4
Zack Bleach <zackbleach@gmail.com>
2015-09-30 16:11:38 -0700

    added dev tools enhancer

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { createStore, combineReducers } from 'redux';
+import { compose, createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
+
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
 
 import CounterApp from './CounterApp';
 import * as reducers from '../reducers';
@@ -8,7 +12,15 @@ import * as reducers from '../reducers';
 import {incrementCounter} from '../actions/Actions';
 
 const reducer = combineReducers(reducers);
-const store = createStore(reducer);
+
+// TODO: refactor this out to own class which switches on config
+const finalCreateStore = compose(
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
+)(createStore);
+
+const store = finalCreateStore(reducer);
+
 
 export default class App extends Component {
   render() {
@@ -17,6 +29,9 @@ export default class App extends Component {
         <Provider store={store}>
           {() => <CounterApp /> }
         </Provider>
+        <DebugPanel top right bottom>
+          <DevTools store={store} monitor={LogMonitor} />
+        </DebugPanel>
       </div>
     );
   }
